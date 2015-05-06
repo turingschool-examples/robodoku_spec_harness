4603afcd02387377903549b6cb8c4bd1e5c2500b
worace <horace.d.williams@gmail.com>
2015-05-06 16:54:10 -0600

    add additional puzzles

--- spec/robodoku_spec.rb
+++ spec/robodoku_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe "Robodoku" do
     it "solves a trivial puzzle" do
       expect(run_puzzle("trivial.txt")).to eq(solution_to("trivial.txt"))
     end
+
+    it "solves an easiest puzzle" do
+      expect(run_puzzle("easiest.txt")).to eq(solution_to("trivial.txt"))
+    end
+
+    it "solves an easy puzzle" do
+      expect(run_puzzle("easiest.txt")).to eq(solution_to("trivial.txt"))
+    end
   end
 end
