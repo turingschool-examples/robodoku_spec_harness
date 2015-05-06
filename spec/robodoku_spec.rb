86ce647a87710fdf9ec38924b03314d2b0c454b9
worace <horace.d.williams@gmail.com>
2015-05-06 17:04:37 -0600

    fix a mistake and add easy2

--- spec/robodoku_spec.rb
+++ spec/robodoku_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe "Robodoku" do
     end
 
     it "solves an easiest puzzle" do
-      expect(run_puzzle("easiest.txt")).to eq(solution_to("trivial.txt"))
+      expect(run_puzzle("easiest.txt")).to eq(solution_to("easiest.txt"))
     end
 
     it "solves an easy puzzle" do
-      expect(run_puzzle("easiest.txt")).to eq(solution_to("trivial.txt"))
+      expect(run_puzzle("easy.txt")).to eq(solution_to("easy.txt"))
+    end
+
+    it "solves another easy puzzle" do
+      expect(run_puzzle("easy2.txt")).to eq(solution_to("easy2.txt"))
     end
   end
 end
